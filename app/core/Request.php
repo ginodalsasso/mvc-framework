62519b1ca2170b125b188da8393a48f8ecb8e328
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-11-12 11:10:02 +0100

    ADD functions get, files, input and all from REQUEST Class

--- app/core/Request.php
+++ app/core/Request.php
@@ -28,9 +28,23 @@
                 return false;
             }
 
-            
+
+            // Retourne la valeur d'une clé dans les données GET ex: $request->get('id')
+            public function get(string $key = '', mixed $default = ''): mixed {
+
+                if(empty($key)) { // Si la clé est vide on retourne toutes les données GET
+
+                    return $_GET; 
+                }else if(isset($_GET[$key])) { // Si la clé existe dans les données GET retournons sa valeur
+
+                    return $_GET[$key];
+                }
+                return $default; // On retourne la valeur par défaut si la clé n'existe pas
+            }
+
+
             // Retourne la valeur d'une clé dans les données POST ex: $request->post('email')
-            public function post(string $key, mixed $default = ''): mixed {
+            public function post(string $key = '', mixed $default = ''): mixed {
 
                 if(empty($key)) { // Si la clé est vide on retourne toutes les données POST
 
@@ -40,5 +54,34 @@
                     return $_POST[$key];
                 }
                 return $default; // On retourne la valeur par défaut si la clé n'existe pas
+            }
+
+
+            // Retourne la valeur d'une clé dans les données FILES ex: $request->files('avatar')
+            public function files(string $key = '', mixed $default = ''): mixed {
+
+                if(empty($key)) { // Si la clé est vide on retourne toutes les données FILES
+
+                    return $_FILES; 
+                }else if(isset($_FILES[$key])) { // Si la clé existe dans les données FILES retournons sa valeur
+
+                    return $_FILES[$key];
+                }
+                return $default; // On retourne la valeur par défaut si la clé n'existe pas
+            }
+
+
+            // Retourne la valeur d'une clé dans les données REQUEST ex: $request->input('email')
+            public function input(string $key, mixed $default = ''): mixed {
+                
+                if(isset($_REQUEST[$key])) { // Si la clé existe dans les données REQUEST retournons sa valeur
+
+                    return $_REQUEST[$key];
+                }
+                return $default; // On retourne la valeur par défaut si la clé n'existe pas
+            }
+
+            public function all():array {
+                return $_REQUEST;
             }
         }
