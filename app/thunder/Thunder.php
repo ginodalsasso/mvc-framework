e560e3a6cb7042b678e5dd87fc5fe49ca56d5a15
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-11-22 18:44:56 +0100

    📦 NEW: Doc and migration:rollback, refresh and list function

--- app/thunder/Thunder.php
+++ app/thunder/Thunder.php
@@ -12,14 +12,21 @@
         private $version = '1.0.0';
 
 
+        /**
+         * Gère diverses opérations liées à la base de données, comme la création, la suppression, 
+         * l'inspection des tables, ou l'exécution de seeders.
+         *
+         * @param array $argv Arguments de la commande passés depuis la ligne de commande.
+         *                    - $argv[1] : Mode d'opération (ex: 'db:create', 'db:table', etc.).
+         *                    - $argv[2] : Nom de la base de données ou de la table selon le mode.
+         */
         public function db($argv){
 
-            $mode =         $argv[1] ?? null; // Récupère le mode
-            $param1 =    $argv[2] ?? null; // Récupère le nom de la classe
+            $mode =         $argv[1] ?? null; 
+            $param1 =    $argv[2] ?? null;
 
             switch ($mode) {
                 case 'db:create':
-                    // Vérifie si param1 est défini
                     if(empty($param1)) 
                         die("\n\rPlease provide a database name\n\r");
                     
@@ -28,10 +35,9 @@
                     $db->query($query);
 
                     die ("\n\rDatabase created successfully!\n\r");
-                    break;
 
+                    break;
                 case 'db:table':
-                    // Vérifie si param1 est défini
                     if(empty($param1)) 
                         die("\n\rPlease provide a table name\n\r");
                     
@@ -47,8 +53,8 @@
                     }
 
                     die ();
-                    break;
 
+                    break;
                 case 'db:drop':
                     // Vérifie si param1 est défini
                     if(empty($param1)) 
@@ -59,25 +65,65 @@
                     $db->query($query);
 
                     die ("\n\rDatabase deleted successfully!\n\r");
-                    break;
 
+                    break;
                 case 'db:seed':
                     echo "\n\rseeder function\n\r";
+
                     break;
-                    
                 default:
                     die("\n\rUnknown command $argv[1]");
+
                     break;
             }
         }
 
 
+        /**
+         * Liste les fichiers de migration du dossier migrations.
+         *
+         * @param array $argv Arguments de la commande passés depuis la ligne de commande.
+         *                    - $argv[1] : Mode d'opération (ex: 'list:migrations').
+         */    
+        public function list($argv){
+            $mode       = $argv[1] ?? null; // Récupère le mode
+    
+            switch ($mode) {
+                case 'list:migrations':
+                    $folder = 'app' .DS. 'migrations' .DS;
+                    if(!file_exists($folder))
+                    {
+                        die("\n\rNo migration files were found\n\r");
+                    }
+    
+                    $files = glob($folder . "*.php");
+                    echo "\n\rMigration files:\n\r";
+    
+                    foreach ($files as $file) {
+                        echo basename($file) ."\n\r";
+                    }
+
+                    break;
+                default:
+                    die("\n\rUnknown command $argv[1]");
+
+                    break;
+            }
+        }
+
+
+        /**
+         * Génère des fichiers pour les contrôleurs, modèles, migrations ou seeders.
+         *
+         * @param array $argv Arguments de la commande passés depuis la ligne de commande.
+         *                    - $argv[1] : Mode d'opération (ex: 'make:controller', 'make:model').
+         *                    - $argv[2] : Nom de la classe ou de l'élément à créer.
+         */
         public function make($argv){
 
-            $mode =         $argv[1] ?? null; // Récupère le mode
-            $classname =    $argv[2] ?? null; // Récupère le nom de la classe
+            $mode =         $argv[1] ?? null; 
+            $classname =    $argv[2] ?? null; 
 
-            // Vérifie si le mode est défini
             if(empty($classname)) 
                 die("\n\rPlease provide a name for the $mode\n\r");
             
@@ -104,8 +150,8 @@
                     } else {
                         die ("\n\rAn error occured while creating $classname\n\r");
                     };
-                    break;
 
+                    break;
                 case 'make:model':
                     // Vérifie si le fichier existe déjà
                     $filename = 'app' .DS. 'models' .DS. ucfirst($classname) .'.php';
@@ -123,8 +169,8 @@
                     } else {
                         die ("\n\rAn error occured while creating $classname\n\r");
                     };
-                    break;
 
+                    break;
                 case 'make:migration':
                     $folder = 'app' .DS. 'migrations' .DS;
                     if(!file_exists($folder)) 
@@ -143,32 +189,62 @@
                     } else {
                         die ("\n\rAn error occured while creating migration file !\n\r");
                     };
-                    break;
 
+                    break;
                 case 'make:seeder':
                     echo "\n\rseeder function\n\r";
-                    break;
 
+                    break;
                 default:
                     die("\n\rUnknown command $argv[1]");
+
                     break;
             }
         }
 
 
+        /**
+         * Exécute des opérations de migration : appliquer, annuler ou rafraîchir une migration spécifique.
+         *
+         * @param array $argv Arguments de la commande passés depuis la ligne de commande.
+         *                    - $argv[1] : Mode d'opération (migrate, migrate:rollback, migrate:refresh).
+         *                    - $argv[2] : Nom du fichier de migration à exécuter.
+         */
         public function migrate($argv){
             $mode =         $argv[1] ?? null;
             $filename =     $argv[2] ?? null; 
             $filename = 'app' .DS. 'migrations' .DS. $filename;
 
             if(file_exists($filename)) {
-                require $filename;
+                require $filename; // Inclut le fichier de migration
 
+                // Extrait le nom de la classe depuis le nom du fichier
                 preg_match("/[a-zA-Z]+\.php$/", $filename, $match);
                 $classname = str_replace(".php", "", $match[0]);
 
-                $myClass = new ("Thunder\\$classname");
-                $myClass->up();
+                $myClass = new ("Thunder\\$classname"); 
+
+                switch ($mode) {              
+                    case 'migrate':
+                        $myClass->up(); 
+                        echo "\n\Tables created successfully!\n\r";
+
+                        break;
+                    case 'migrate:rollback': // Annuler la migration
+                        $myClass->down(); 
+                        echo "\n\Tables dropped successfully!\n\r";
+
+                        break;
+                    case 'migrate:refresh': // Rafraîchir la migration
+                        $myClass->down(); 
+                        $myClass->up(); 
+                        echo "\n\Tables refreshed successfully!\n\r";
+
+                        break;
+                    default:
+                        $myClass->down();
+                        break;
+                }
             } else {
                 die("\n\rMigration file not found!\n\r");
             }
@@ -186,9 +262,9 @@
                     db:seed            Seed the database with records.
                     db:table           Retrieves information on the selected table.
                     db:drop            Drop/Delete the database schema.
-                    migrate            Locate and runs a migration from the specifed plugin folder.
-                    migrate:refresh    Does a rollback followed by a latest to refresh the current state of the database.
-                    migrate:rollback   Runs the 'down'  method for a migration in the specified plugin folder.
+                    migrate            Locate and runs a migration file.
+                    migrate:refresh    Runs the 'down' and then 'up' method for a migration file.
+                    migrate:rollback   Runs the 'down'  method for a migration file.
                     
                 Generators
                     make:controller    Create a new controller file.
--- app/thunder/samples/migration-sample.php
+++ app/thunder/samples/migration-sample.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Classe de migration pour {CLASSNAME}.
+ *
+ * Cette classe gère la création et la suppression de la table `{classname}` dans la base de données.
+ * 
+ * Méthodes :
+ * - up() : Méthode appelée lors de la migration pour créer la table et insérer des données.
+ * - down() : Méthode appelée lors de la rétrogradation pour supprimer la table.
+ *
+ * Exemple d'utilisation :
+ * $this->addColumn('id int(11) NOT NULL AUTO_INCREMENT');
+ * $this->addColumn('date_created datetime NULL');
+ * $this->addColumn('date_updated datetime NULL');
+ * $this->addPrimaryKeys('id');
+ * $this->createTable('{classname}');
+ * $this->addData('date_created', date("Y-m-d H:i:s"));
+ * $this->addData('date_updated', date("Y-m-d H:i:s"));
+ * $this->insertData('{classname}');
+ */
 
     namespace Thunder;
 
@@ -6,40 +25,22 @@
 
     class {CLASSNAME} extends Migration {
 
-        use Migration; // 
-
-
         public function up() {  
             // Méthodes autorisées
-            /*
-            $this->addColumn();
-            $this->addPrimaryKey();
-            $this->addUniqueKey();
-            // $this->addForeignKey();
+            $this->addColumn(); // Ajouter une colonne à la table.
+            $this->addPrimaryKey(); // Définir une clé primaire pour la table. 
+            $this->addUniqueKey(); //  Définir une clé unique pour la table.
+            $this->addForeignKey(); // 
 
-            $this->addData();
-            $this->insertData();
-            $this->createTable();
-            */
-
-            /** Creer une table **/
-            $this->addColumn('id int(11) NOT NULL AUTO_INCREMENT');
-            $this->addColumn('date_created datetime NULL');
-            $this->addColumn('date_updated datetime NULL');
-            $this->addPrimaryKey('id');
-
-            $this->createTable('{classname}');
-
-            /** insert data **/
-            $this->addData('date_created',date("Y-m-d H:i:s"));
-            $this->addData('date_updated',date("Y-m-d H:i:s"));
-
-            $this->insertData('{classname}');
+            $this->addData(); // Ajouter des données à insérer dans la table.
+            $this->insertData(); // Insérer des données dans la table.
+            $this->createTable(); // Créer la table.
+            
         }
 
 
         public function down() {
-            $this->dropTable('{classname}');
+            $this->dropTable('{classname}'); // Supprimer la table.
         }
         
     }
